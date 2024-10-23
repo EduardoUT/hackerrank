cccc453b40ceb98279bc2b89cf258d6111dcd188
Eduardo Reyes Hernández <mcorey316@gmail.com>
2024-10-22 18:42:54 -0600

    Creando clase para Insert Sort Parte 1

---
+++ src/main/java/com/mycompany/hackerrank/sortingalgotirhms/InsertionSortPartOne.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.hackerrank.sortingalgotirhms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * Insertion sort simple, taking the last value in the array and comparing to
+ * the rest of elements, replace the element less than the last elemento stored.
+ *
+ * @author eduar
+ */
+public class InsertionSortPartOne {
+
+    public static void main(String[] args) {
+        List<Integer> arr = new ArrayList<>();
+        arr.add(1);
+        arr.add(2);
+        arr.add(4);
+        arr.add(5);
+        arr.add(3);
+        insertionSort1(arr.size(), arr);
+    }
+
+    public static void insertionSort1(int n, List<Integer> arr) {
+        int unsortedValue = arr.get(n - 1);
+        for (int i = n - 2; i >= 0; i--) {
+            int currentValue = arr.get(i);
+            if (currentValue < unsortedValue) {
+                arr.set(i + 1, unsortedValue);
+                print(arr);
+                break;
+            } else {
+                arr.set(i + 1, arr.get(i));
+                print(arr);
+                if (i == 0) {
+                    arr.set(i, unsortedValue);
+                    print(arr);
+                }
+            }
+        }
+    }
+
+    public static void print(List<Integer> arr) {
+        String path = "[\\[\\]\\,]";
+        String result = arr.toString().replaceAll(path, "");
+        System.out.println(result);
+    }
+}
